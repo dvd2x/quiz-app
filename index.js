9f3483dfe899ec8b83d35b2209cdf1c430e2c97e
David Piper <black.god1930@gmail.com>
2019-04-16 15:01:05 -0400

    finnished Quiz App

--- index.js
+++ index.js
@@ -1,127 +1,165 @@
-"use strict";
-
-const QUIZ = [
+const questions = [
   {
-    number: 1,
-    question: "How many feet in a meter stick?",
-    answer1: "3ft 3in",
-    answer2: "3ft",
-    answer3: "22in",
-    answer4: "5yards",
-    question: "How many inches in a foot?",
-    number: 2,
-    answer1: "24in",
-    answer2: "36in",
-    answer3: "12in",
-    answer4: "15in",
-    question:
-      "If Hannah had 19.27$ and Riley had 13.59$ how much more money does Hannah have?",
-    number: 3,
-    answer1: 6.32,
-    answer2: 1.68,
-    answer3: 5.61,
-    answer4: 5.68,
-    question: "How many right angels in a square?",
-    number: 4,
-    answer1: 4,
-    answer2: 3,
-    answer3: 5,
-    answer4: 1,
-    question: "Is 656 a prime number?",
-    number: 5,
-    answer1: true,
-    answer2: false,
-    question: "If you had 785 dogs how many would you have if I took 524?",
-    number: 6,
-    answer1: 361,
-    answer2: 451,
-    answer3: 261,
-    answer4: 275,
-    question:
+    title: "How many feet in a meter stick?",
+    answers: ["3ft 3in", "3ft", "22in", "5yards"],
+    correct: 0
+  },
+  {
+    title: "How many inches in a foot?",
+    answers: ["24in", "36in", "12in", "15in"],
+    correct: 2
+  },
+  {
+    title: "Among the following which natural number has no predecessor?",
+    answers: [100, 200, 1, 0],
+    correct: 2
+  },
+  {
+    title:
+      "Hannah had $19.27 and Riley had $13.59 how much more money does Hannah have?",
+    answers: [6.32, 1.68, 5.61, 5.68],
+    correct: 3
+  },
+  {
+    title: "How many right angels in a square?",
+    answers: [4, 3, 5, 1],
+    correct: 0
+  },
+  {
+    title: "Is 656 a prime number?",
+    answers: [true, false],
+    correct: 1
+  },
+  {
+    title: "If you had 785 dogs how many would you have if I took 524?",
+    answers: [361, 451, 261, 275],
+    correct: 2
+  },
+  {
+    title:
       "Harmony had 357 cats and Lindsey had 150 dogs so how many animals are there altogether?",
-    number: 7,
-    answer1: 507,
-    answer2: 510,
-    answer3: 432,
-    answer4: 513,
-    question:
+    answers: [507, 510, 432, 513],
+    correct: 0
+  },
+  {
+    title:
       "Julian had 52 webkinz and Bria had 169. How many are there altogether?",
-    number: 8,
-    answer1: 231,
-    answer2: 201,
-    answer3: 221,
-    answer4: 220,
-    question:
+    answers: [231, 201, 221, 220],
+    correct: 2
+  },
+  {
+    title:
       "If you want to go to Gurnee Mills and it's 1 hour from your house but you have to wait an extra hour how many hours did t take you to get to Gurnee Mills?",
-    number: 9,
-    answer1: "2 hours",
-    answer2: "3 hours",
-    answer3: "1,5 hours",
-    answer4: "1 hour",
-    question:
-      "45,269 people died last year (not real) and if 52,036 died this year how many people died in the two years?",
-    number: 10,
-    answer1: 97305,
-    answer2: 98000,
-    answer3: 93445,
-    answer4: 97856
+    answers: ["2 hours", "3 hours", "1,5 hours", "1 hour"],
+    correct: 0
   }
 ];
 
-const ANSWERS = [
-  "3ft 3in",
-  "12in",
-  5.68,
-  4,
-  false,
-  261,
-  507,
-  221,
-  "2 hours",
-  97305
-];
+let guess;
+let score = 0;
+let currentQuestion = 0;
 
-function answersGiven(ask) {
-  return `<li>
-  <span class="questions">${ask.question}</span> 
-</li>
-<li>
-  <span class="questions">${ask.answer1}</span>
-</li>
-<li>
-  <span class="questions">${ask.answer3}</span>
-</li>
-<li>
-  <span class="questions">${ask.answer4}</span>
-</li>
-<li>
-  <span class="questions">${ask.answer1}</span>
-</li>
-<li>
-  <span class="questions">${ask.answer2}</span>
-</li>
-<li>
-  <span class="questions">${ask.answer3}</span>
-</li>
-<li>
-  <span class="questions">${ask.answer1}</span>
-</li>
-<li>
-  <span class="questions">${ask.answer3}</span>
-</li>
-<li>
-  <span class="questions">${ask.answer1}</span>
-</li>
-<li>
-  <span class="questions">${ask.answer1}</span>
-</li>
-<li class="button-control">
-  <div class="quiz-control">
-    <button class="start-quiz">
-      <span class="button-label">Let's Begin</span>
-    </button>
-    <button type="submit" class="next-question">NEXT</button>
-  </li>`;
+//math quiz starts once #startQuiz button is hit, this hides the welcome page along with button and shows the quiz.
+$(function() {
+  $(".start #startQuiz").on("click", function(e) {
+    e.preventDefault();
+    $(".start").hide();
+    $("footer").show();
+    $(".question-circle").show();
+    $(".score-circle").show();
+    $(".quiz").show();
+    showQuestion(); //<- shows question
+    questionStatus();
+    scoreStatus();
+  });
+
+  //this eventListner will allow you to addClass selected for your answers and will preform an event deligation
+  $(".quiz ul").on("click", "li", function() {
+    $(".selected").removeClass("selected");
+    $(this).addClass("selected");
+  });
+
+  //this eventListner will allow you to submit your answer, but if you submit answer before you choose an answer an alert will be shown to user.
+  $(".quiz #submitAnswer").click(function(e) {
+    e.preventDefault();
+    if ($("li.selected").length) {
+      let guess = parseInt($("li.selected").attr("id"));
+      checkAnswer(guess);
+    } else {
+      alert("Please select an answer");
+    }
+  });
+
+  //after you finnish your quiz this eventListner restartQuiz will restart the entire quiz brining you back to the welcome screen
+  $(".summary #restartQuiz").click(function(e) {
+    e.preventDefault();
+    restartQuiz();
+  });
+});
+
+//this function shows questions by taking the class quiz questions and answers and injecting them into ul/li
+function showQuestion() {
+  let question = questions[currentQuestion];
+  $(".quiz h2").text(question.title);
+  $(".quiz ul").html("");
+  for (let i = 0; i < question.answers.length; i++) {
+    $(".quiz ul").append(`<li id="${i}">${question.answers[i]}</li>`);
+  }
 }
 
-function generateQuestions(params) {}
+//this functions checks and scores your quiz
+function checkAnswer(guess) {
+  let question = questions[currentQuestion];
+  if (question.correct === guess) {
+    alert("CORRECT!!! 👍😁😁😁👍");
+    score++;
+  } else {
+    alert(`WRONG!!!👎😠😠😠👎 ${question.answers[question.correct]}`);
+  }
+  currentQuestion++; //<- this currentQuestion increments to your next question
+  if (currentQuestion >= questions.length) {
+    showSummary();
+  } else {
+    questionStatus();
+    scoreStatus();
+    showQuestion();
+  }
+}
+
+//this function shows your score while taking the quiz
+function showSummary() {
+  $(".quiz").hide();
+  clearStatus();
+  $(".summary").show();
+  $(".summary p").text(
+    `Congrats you scored ${score} out of ${questions.length} correct!`
+  );
+}
+
+//restarts quiz when finnished
+function restartQuiz() {
+  $(".summary").hide();
+  $(".start").show();
+  score = 0;
+  currentQuestion = 0;
+  showQuestion();
+}
+/////////////////////////////////////
+function questionStatus() {
+  $(".question-circle").empty();
+  $(".question-circle").append(
+    `<p class= 'question-status'>Question ${currentQuestion + 1}/10</p>`
+  );
+}
+
+function scoreStatus() {
+  $(".score-circle").empty();
+  $(".score-circle").append(`<p>Score ${score}/10</p>`);
+  console.log("scoreStatus ran");
+  console.log(score);
+}
+
+function clearStatus() {
+  $(".question-circle").empty();
+  $(".score-circle").empty();
+}
